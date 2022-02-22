53be15f7a7bcdec2a96456e834d738b5065a5575
uMetalooper <unityloper@gmail.com>
2022-02-22 23:09:34 +0000

    Toggle reminder and conditional styling

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -7,16 +7,36 @@ function App() {
     {
       id: 1,
       text: "abc",
+      day: "2020",
+      reminder: true,
     },
     {
       id: 2,
       text: "xyz",
+      day: "2021",
+      reminder: false,
     },
   ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,8 +1,15 @@
-const Tasks = ({ tasks }) => {
+import Task from "./Task";
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <div>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
